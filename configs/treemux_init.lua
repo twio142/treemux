3804ff3beb0bed54f4d7e4ee727395435ff74d9c
Kiyoon Kim <yoonkr33@gmail.com>
2023-01-31 04:01:33 +0000

    nvim tree not opening fix

--- configs/treemux_init.lua
+++ configs/treemux_init.lua
@@ -137,3 +137,26 @@ require("lazy").setup {
 
 vim.cmd [[ colorscheme tokyonight-night ]]
 vim.o.cursorline = true
+
+-- Open nvim-tree when entering a directory
+-- Required for treemux
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_augroup("nvim_tree_open", {})
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = open_nvim_tree,
+  group = "nvim_tree_open",
+})
